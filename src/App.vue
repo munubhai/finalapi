de1ec163fc2ee9ebbddbf1b4e81362e45d669252
munubhai <munubhai@users.noreply.github.com>
2022-03-25 11:11:13 -0400

    main

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,11 @@
   <div id="app">
     <h2>Vue WebAPI</h2>
     <CoinGecko></CoinGecko>
+    <p>Data source <a href="https://api.coingecko.com/api/v3/coins/markets"><code>https://api.coingecko.com/api/v3/coins/markets</code></a></p>
+
     <AnimeList></AnimeList>
+    <p>Data source <a href="https://anime-facts-rest-api.herokuapp.com/api/v1"><code>https://anime-facts-rest-api.herokuapp.com/api/v1</code></a></p>
+
     <!--<img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/> -->
   </div>
